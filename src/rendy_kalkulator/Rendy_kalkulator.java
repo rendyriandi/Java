5aede53718a06dda097cc0e03700a4d5f3100959
rendy <rendy9936@gmail.com>
2019-05-21 15:34:53 +0700

    ui

---
+++ src/rendy_kalkulator/Rendy_kalkulator.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rendy_kalkulator;
+
+/**
+ *
+ * @author rendy
+ */
+public class Rendy_kalkulator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
